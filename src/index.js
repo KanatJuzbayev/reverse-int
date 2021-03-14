dbaa948935cc7a791f14d3a1fd40b3ee436355e3
Kanat Juzbayev <76990478+KanatJuzbayev@users.noreply.github.com>
2021-03-14 21:58:10 +0600

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    var strN = n.toString().split('');
+    let rn = strN.reverse();
+
+    var str = '';
+    rn.forEach((x) => {
+    str = str + x;
+    })
+
+    let rNum = parseInt(str);
+
+    return rNum;
 }
